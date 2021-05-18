5ee526e64165fa46516babc408fdc9caef3b7e4c
akashkrishnak <70852245+akashkrishnak@users.noreply.github.com>
2021-05-18 21:02:49 +0530

    added map

--- src/screens/LocateScreen.js
+++ src/screens/LocateScreen.js
@@ -1,22 +1,20 @@
 import React from "react";
 import { View, Text, Image, StyleSheet } from "react-native";
 import Call from "../components/Call";
-/* import Map from "../Components/Map"; */
-/* import useLocation from "../hooks/useLocation"; */
+import Map from "../Components/Map"; 
+import useLocation from "../hooks/useLocation"; 
 
-const LocateScreen = () => {
-  return (
-    <View style={styles.container}>
-      <Image style={styles.img} source={require("../img/pinkpal.png")} />
-      {/* <Map />
-      {errorMsg ? <Text>Please enable location services</Text> : null} */}
-      <Text style={styles.txt}>
-        Your location has been tracked by the police and nearest volunteers.
-        Help is on the way
-      </Text>
-      <Call />
-    </View>
-  );
+const LocationScreen = ({ isFocused}) => {
+    const [errorMsg] = useLocation(isFocused);
+
+    return (
+       <View style={styles.container}>
+         <Image style={styles.img} source={require('../img/pinkpal.png')} /> 
+         <Map />
+         {errorMsg ? <Text>Please enable location services</Text> : null}
+         <Call />
+       </View>
+    );
 };
 
 const styles = StyleSheet.create({
